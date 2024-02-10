bff35d1393c554e74a736029266826a137c29f83
Ezebed <obedvillegas.02@gmail.com>
2024-02-10 15:44:23 -0400

    database configuration

--- soluciones-online-interview/app/Models/Chat.php
+++ soluciones-online-interview/app/Models/Chat.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Elocuent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Chat extends Model
 {
@@ -13,4 +15,14 @@ class Chat extends Model
         'userA_id',
         'userB_id',
     ];
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function User(): HasOne
+    {
+        return $this->hasOne(User::class);
+    }
 }
--- soluciones-online-interview/app/Models/Message.php
+++ soluciones-online-interview/app/Models/Message.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'chat_id',
+        'sender_id',
+        'body',
+    ];
+
+    public function chat():BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function User():HasOne
+    {
+        return $this->hasOne(User::class,'sender_id');
+    }
 }
